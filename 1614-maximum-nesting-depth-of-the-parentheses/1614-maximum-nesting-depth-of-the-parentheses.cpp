5143affdb087a5bfe2275c1cc0a55feb3394e31d
Ankita Paul <167278492+ankitapaul74@users.noreply.github.com>
2024-12-04 11:16:52 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (6.82%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+       int n=s.length();
+       stack<char> st;
+       int ans=0;
+       for(int i=0;i<n;i++){
+        if(s[i]=='('){
+            st.push(s[i]);
+        }else if(s[i]==')'){
+            st.pop();
+        }
+         ans=max(ans,(int)st.size());
+       }
+       return ans;
+    }
+};
