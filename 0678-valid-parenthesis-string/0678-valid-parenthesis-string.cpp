66fe3d73ede904fcc1195dd58117fba7770f2a2a
Ankita Paul <167278492+ankitapaul74@users.noreply.github.com>
2024-12-09 10:42:25 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (24.82%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n=s.length();
+        int low=0,high=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                low++;
+                high++;
+            }else if(s[i]==')'){
+                low=max(0,low-1);
+                high--;
+            }else{
+                low=max(0,low-1);
+                high++;
+            }
+            if(high<0){
+                return false;
+            }
+        }
+        return (low==0);
+    }
+};
